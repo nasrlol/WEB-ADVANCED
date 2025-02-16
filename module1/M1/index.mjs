009af36029acd2f7ac713751d9f156d1fad33770
nasrlol <nsrddyn@gmail.com>
2025-02-16 22:56:09 +0100

    een beetje gewerkt aan m1

---
+++ module1/M1/index.mjs
@@ -0,0 +1,19 @@
+"use strict";
+
+// text van special class naar rood
+let special = document.getElementsByTagName("span");
+for (let i = 0; i < special.length; i++) {
+	special[i].style.color = "red";
+}
+
+// tweede paragraaf onderstrepen
+let paragraaf = document.getElementsByTagName("p");
+if (paragraaf.length > 1) paragraaf[0].style.textDecoration = "underline";
+
+let count = 0;
+for (let i = 0; i < document.querySelectorAll("special").length; i++) {
+    console.log('nice');
+	count++;
+}
+
+console.log(count);
