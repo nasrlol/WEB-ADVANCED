a6d681787865e40ce697341cd78bd2c198f364df
Abdellah El Morabit <nsrddyn@gmail.com>
2025-02-24 23:01:58 +0100

    bezig aan E2 M2

--- module2/E2/index.mjs
+++ module2/E2/index.mjs
@@ -1,2 +1,13 @@
-'use strict';
+"use strict";
 
+const receptNaam = document.getElementById("recipeName").value;
+receptNaam;
+const bereidTijd = document.getElementById("prepTime").textContent;
+bereidTijd;
+
+const ingr = document.getElement;
+document.getElementById("generateCard").addEventListener("click", () => {});
+
+function generate() {
+	document.getElementById("result").textContent = "";
+}
