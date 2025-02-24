61163e60d225579972a26f1872ff8aeee9cff286
Abdellah El Morabit <nsrddyn@gmail.com>
2025-02-24 10:28:20 +0100

    ondertitels positioneren werkt inderdaad beter met een nested for loop, nu nog alleen de hoeveelheid, en dan de href checken

--- module1/H1/index.mjs
+++ module1/H1/index.mjs
@@ -6,14 +6,23 @@ const H2Count = document.getElementsByTagName("h2");
 const NAV = document.getElementById("mainMenu");
 
 for (let i = 0; i < H1Count.length; i++) {
+	const H1 = H1Count[i];
+	const id = `Hoofdstuk ${[i + 1]}`;
+	H1.id = id;
+
+	const clickableLink = document.createElement("a");
+	clickableLink.href = `#${H1}`;
+	clickableLink.innerText = `Hoofdstuk ${[i + 1]}`;
+	NAV.append(clickableLink);
+
 	for (let j = 0; j < H2Count.length; j++) {
-		const H2 = H2Count[i];
-		const id = `deel-${i + 1}`;
+		const H2 = H2Count[j];
+		const id = `deel-${j + 1}`;
 		H2.id = id;
 
 		const clickableLink = document.createElement("a");
-		clickableLink.href = `#${id}`;
-		clickableLink.innerText = `Deel ${j}.${i + 1}`;
+		clickableLink.href = `#${H2}`;
+		clickableLink.innerText = `Deel ${i + 1}.${j + 1}`;
 
 		NAV.append(clickableLink);
 	}
