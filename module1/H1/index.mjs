b9f2bbfbbfc838bfdafeb61367a24d3a9fd7969a
Abdellah El Morabit <nsrddyn@gmail.com>
2025-02-24 10:14:57 +0100

    nog altijd bezig met module1 H1, de links generen met js gaat wat moeizaam precies

--- module1/H1/index.mjs
+++ module1/H1/index.mjs
@@ -6,19 +6,25 @@ const H2Count = document.getElementsByTagName("h2");
 const NAV = document.getElementById("mainMenu");
 
 for (let i = 0; i < H1Count.length; i++) {
-	const H1 = document.createElement("h1");
-	const clickableLink = document.createElement("a");
-	const clickableH1 = clickableLink.appendChild(H1);
+	const H1 = H1Count[i];
+	const id = `hoofdstuk${i + 1}`;
+	H1.id = id;
 
-	NAV.append(clickableH1);
-	clickableH1.innerText = `Hoofdstuk ${i}`;
+	const clickableLink = document.createElement("a");
+	clickableLink.href = `#${id}`;
+	clickableLink.innerText = `Hoofdstuk ${i + 1}`;
+
+	NAV.append(clickableLink);
 }
 
 for (let i = 0; i < H2Count.length; i++) {
-	const H2 = document.createElement("h2");
-	const clickableLink = document.createElement("a");
-	const clickableH2 = clickableLink.appendChild(H2);
+	const H2 = H2Count[i];
+	const id = `deel-${i + 1}`;
+	H2.id = id;
 
-	NAV.append(clickableH2);
-	clickableH2.innerText = `Deel 1.${i}`;
+	const clickableLink = document.createElement("a");
+	clickableLink.href = `#${id}`;
+	clickableLink.innerText = `Deel 1.${i + 1}`;
+
+	NAV.append(clickableLink);
 }
