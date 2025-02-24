654d214874f1488324756aee5ae1d48e3de54db0
Abdellah El Morabit <nsrddyn@gmail.com>
2025-02-24 10:17:43 +0100

    nested for loop?

--- module1/H1/index.mjs
+++ module1/H1/index.mjs
@@ -6,25 +6,15 @@ const H2Count = document.getElementsByTagName("h2");
 const NAV = document.getElementById("mainMenu");
 
 for (let i = 0; i < H1Count.length; i++) {
-	const H1 = H1Count[i];
-	const id = `hoofdstuk${i + 1}`;
-	H1.id = id;
+	for (let j = 0; j < H2Count.length; j++) {
+		const H2 = H2Count[i];
+		const id = `deel-${i + 1}`;
+		H2.id = id;
 
-	const clickableLink = document.createElement("a");
-	clickableLink.href = `#${id}`;
-	clickableLink.innerText = `Hoofdstuk ${i + 1}`;
+		const clickableLink = document.createElement("a");
+		clickableLink.href = `#${id}`;
+		clickableLink.innerText = `Deel ${j}.${i + 1}`;
 
-	NAV.append(clickableLink);
-}
-
-for (let i = 0; i < H2Count.length; i++) {
-	const H2 = H2Count[i];
-	const id = `deel-${i + 1}`;
-	H2.id = id;
-
-	const clickableLink = document.createElement("a");
-	clickableLink.href = `#${id}`;
-	clickableLink.innerText = `Deel 1.${i + 1}`;
-
-	NAV.append(clickableLink);
+		NAV.append(clickableLink);
+	}
 }
